ab26698c54f259b4e2d4c907f6013125728f7304
Bryan Ramos Vargas <matematica_bryan@hotmail.com>
2022-11-27 23:00:10 -0500

    about editado

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -17,12 +17,11 @@ export default function About() {
         curioso, apasionado por la <b>programación</b>. <br />
         Dispuesto a
         aprender, debatir soluciones, compartir conocimientos, afrontar nuevos
-        retos, trabajar en equipo y tengo autonomía para crecer tanto{" "}
-        <b>personal</b> como <b>profesionalmente</b>. <br />
+        retos, trabajar en equipo y tengo autonomía para crecer <b>profesionalmente</b>. <br />
         Me enfoco en crear experiencias <b>buenas y fluídas</b> entre el{" "}
         <b>producto y usuario</b>. <br />
         Considero que me falta mucho por crecer como <b>persona</b> y{" "}
-        <b>profesionalmente</b>, pero poco a poco apuntando hacia arriba. <br />
+        <b>profesional</b>, pero poco a poco apuntando hacia arriba. <br />
         Mis metas son: Apoyar a mi <b>familia</b> y vivir{" "}
         <b>haciendo lo que me gusta</b>.
       </p>
